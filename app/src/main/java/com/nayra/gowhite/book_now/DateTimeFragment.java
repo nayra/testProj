2c275b1dfe011dccbe2f68a0f08fb90c76597e80
nayrael-sayed <123nany>
2018-02-18 01:20:22 +0200

    ROLL BACK TO CALENDAR VIEW

--- app/src/main/java/com/nayra/gowhite/book_now/DateTimeFragment.java
+++ app/src/main/java/com/nayra/gowhite/book_now/DateTimeFragment.java
@@ -4,12 +4,10 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.DatePicker;
 
 import com.nayra.gowhite.R;
 import com.nayra.gowhite.databinding.FragmentDateTimeBinding;
@@ -23,7 +21,7 @@ import java.util.Calendar;
 public class DateTimeFragment extends Fragment {
 
     private FragmentDateTimeBinding binding;
-    private String selected_time = "08:00", selected_date = "";
+    private String selected_time = "08:00";
 
     @Nullable
     @Override
@@ -35,35 +33,9 @@ public class DateTimeFragment extends Fragment {
 
     private void initView() {
         final Calendar calendar = Calendar.getInstance();
+        binding.calendarView.setDate(calendar.getTimeInMillis());
+        binding.calendarView.setMinDate(calendar.getTimeInMillis());
 
-        selected_date = Utils.getDate(calendar.getTimeInMillis(), "dd-MM-yyyy");
-
-        binding.datePicker.setMinDate(calendar.getTimeInMillis());
-        binding.datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
-                String y = String.valueOf(year);
-                month += 1;
-
-                String m;
-                if (month < 10) {
-                    m = "0" + String.valueOf(month);
-                } else {
-                    m = String.valueOf(month);
-                }
-
-                String d;
-
-                if (day < 10)
-                    d = "0" + String.valueOf(day);
-                else {
-                    d = String.valueOf(day);
-                }
-                selected_date = d + "-" + m + "-" + y;
-
-                Log.d("DateTimeFragment", selected_date);
-            }
-        });
         final String[] times = getActivity().getResources().getStringArray(R.array.times);
         binding.spTimes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -85,8 +57,8 @@ public class DateTimeFragment extends Fragment {
 
 
     public void getDateAndTimeDetails() {
-        // final long milliseconds = 0;// binding.calendarView.getSelectedDate().getCalendar().getTimeInMillis();
-        String date = selected_date;
+        final long milliseconds = binding.calendarView.getDate();
+        String date = Utils.getDate(milliseconds, "dd-MM-yyyy");
         date += " " + selected_time;
         BookNowActivity.appointmentDetails.setStartDate(date);
     }
